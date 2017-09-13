e8dd684dcc54e23f2c17efb9e64c27198511eedd
Fabian Wetzel <fabsenet@googlemail.com>
2017-09-13 17:20:09 +0200

    converting to xaml

--- adrilight/App.xaml.cs
+++ adrilight/App.xaml.cs
@@ -1,26 +1,35 @@
-﻿using Microsoft.Win32;
+﻿using adrilight.ui;
+using Microsoft.Win32;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
 using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading;
-using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Windows;
 
-namespace adrilight {
+using AdriSettings = adrilight.Properties.Settings;
 
-    static class Program {
-
+namespace adrilight
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
         private static readonly ILogger _log = LogManager.GetCurrentClassLogger();
 
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        protected override void OnStartup(StartupEventArgs startupEvent)
         {
+            base.OnStartup(startupEvent);
+
 #if DEBUG
             var config = new LoggingConfiguration();
             var debuggerTarget = new DebuggerTarget() { Layout = "${processtime} ${message:exceptionSeparator=\n\t:withException=true}" };
@@ -29,36 +38,33 @@ namespace adrilight {
 
             LogManager.Configuration = config;
 #endif
-
             _log.Debug($"adrilight {VersionNumber}: Main() started.");
 
             AppDomain.CurrentDomain.UnhandledException +=
-                (sender, args) => ApplicationOnThreadException(sender, args.ExceptionObject as Exception);
+    (sender, args) => ApplicationOnThreadException(sender, args.ExceptionObject as Exception);
 
             Settings.Load();
-            
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.ThreadException += (sender, args) => ApplicationOnThreadException(sender, args.Exception);
 
-            Application.ApplicationExit += (s, e) => _log.Debug("Application exit!");
+            DispatcherUnhandledException += (sender, args) => ApplicationOnThreadException(sender, args.Exception);
+
+            Exit += (s, e) => _log.Debug("Application exit!");
             SystemEvents.PowerModeChanged += (s, e) => _log.Debug("Changing Powermode to {0}", e.Mode);
 
             SetupNotifyIcon();
 
             //subscribe for changes in the settings
-            Properties.Settings.Default.PropertyChanged += (s, e) => SpotSet.Refresh();
+            AdriSettings.Default.PropertyChanged += (s, e) => SpotSet.Refresh();
             //exeucte once to setup the leds
             SpotSet.Refresh();
 
             //subscribe for changes in the settings
-            Properties.Settings.Default.PropertyChanged += (s, e) => RefreshCapturingState();
+            AdriSettings.Default.PropertyChanged += (s, e) => RefreshCapturingState();
             //exeucte once to start the capturing initially
             RefreshCapturingState();
 
 
             //subscribe for changes in the settings
-            Properties.Settings.Default.PropertyChanged += (s, e) => RefreshTransferState();
+            AdriSettings.Default.PropertyChanged += (s, e) => RefreshTransferState();
             //exeucte once to start the serial stream initially
             RefreshTransferState();
 
@@ -67,92 +73,33 @@ namespace adrilight {
             {
                 OpenSettingsWindow();
             }
-            Application.Run();
-        }
-        
-        private static NotifyIcon SetupNotifyIcon()
-        {
-            var icon = new System.Drawing.Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream("adrilight.adrilight_icon.ico"));
-            var contextMenu = new ContextMenu();
-            contextMenu.MenuItems.Add(new MenuItem("Settings...", (s, e) => OpenSettingsWindow()));
-            contextMenu.MenuItems.Add(new MenuItem("Exit", (s, e) => Application.Exit()));
-
-            var notifyIcon = new NotifyIcon()
-            {
-                Text = $"adrilight {GetVersionNumber()}",
-                Icon = icon,
-                Visible = true,
-                ContextMenu =contextMenu
-            };
-            notifyIcon.DoubleClick += (s, e) => { OpenSettingsWindow(); };
-
-            Application.ApplicationExit += (s, e) => notifyIcon.Dispose();
-
-            return notifyIcon;
         }
 
-        static MainForm _mainForm;
-        private static void OpenSettingsWindow()
+
+        SettingsWindow _mainForm;
+        private void OpenSettingsWindow()
         {
-            if(_mainForm == null)
+            if (_mainForm == null)
             {
-                _mainForm = new MainForm();
-                _mainForm.FormClosed += MainForm_FormClosed;
+                _mainForm = new SettingsWindow();
+                _mainForm.Closed += MainForm_FormClosed;
                 _mainForm.Show();
             }
             else
             {
-                _mainForm.BringToFront();
+                //bring to front?
+                _mainForm.Focus();
             }
         }
 
-        private static void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        private void MainForm_FormClosed(object sender, EventArgs e)
         {
             if (_mainForm == null) return;
 
             //deregister to avoid memory leak
-            _mainForm.FormClosed -= MainForm_FormClosed;
+            _mainForm.Closed -= MainForm_FormClosed;
             _mainForm = null;
         }
-
-        private static void ApplicationOnThreadException(object sender, Exception ex)
-        {
-            _log.Fatal(ex, $"ApplicationOnThreadException from sender={sender}, adrilight version={VersionNumber}");
-
-            var sb = new StringBuilder();
-            sb.AppendLine($"Sender: {sender}");
-            if (sender != null)
-            {
-                sb.AppendLine($"Sender Type: {sender.GetType().FullName}");
-            }
-            sb.AppendLine("-------");
-            do
-            {
-                sb.AppendLine($"exception type: {ex.GetType().FullName}");
-                sb.AppendLine($"exception message: {ex.Message}");
-                sb.AppendLine($"exception stacktrace: {ex.StackTrace}");
-                sb.AppendLine("-------");
-                ex = ex.InnerException;
-            } while (ex != null);
-
-            MessageBox.Show(sb.ToString(), "unhandled exception :-(");
-            Application.Exit();
-        }
-
-        public static string VersionNumber { get; } = GetVersionNumber();
-
-        private static string GetVersionNumber()
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-            string currentVersion;
-            using (var versionStream = assembly.GetManifestResourceStream("adrilight.version.txt"))
-            {
-                currentVersion = new StreamReader(versionStream).ReadToEnd().Trim();
-            }
-            return currentVersion;
-        }
-
-
 
         private static DesktopDuplicatorReader _desktopDuplicatorReader;
         private static CancellationTokenSource _cancellationTokenSource;
@@ -209,6 +156,64 @@ namespace adrilight {
                 _log.Debug("stopping the serial stream");
                 _mSerialStream.Stop();
             }
+        }
+
+        private void SetupNotifyIcon()
+        {
+            var icon = new System.Drawing.Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream("adrilight.adrilight_icon.ico"));
+            var contextMenu = new System.Windows.Forms.ContextMenu();
+            contextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("Settings...", (s, e) => OpenSettingsWindow()));
+            contextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("Exit", (s, e) => Shutdown(0)));
+
+            var notifyIcon = new System.Windows.Forms.NotifyIcon()
+            {
+                Text = $"adrilight {GetVersionNumber()}",
+                Icon = icon,
+                Visible = true,
+                ContextMenu = contextMenu
+            };
+            notifyIcon.DoubleClick += (s, e) => { OpenSettingsWindow(); };
+            
+            Exit += (s, e) => notifyIcon.Dispose();
+        }
+
+
+        public static string VersionNumber { get; } = GetVersionNumber();
+
+        private static string GetVersionNumber()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            string currentVersion;
+            using (var versionStream = assembly.GetManifestResourceStream("adrilight.version.txt"))
+            {
+                currentVersion = new StreamReader(versionStream).ReadToEnd().Trim();
+            }
+            return currentVersion;
+        }
+
+
+        private void ApplicationOnThreadException(object sender, Exception ex)
+        {
+            _log.Fatal(ex, $"DispatcherUnhandledException from sender={sender}, adrilight version={VersionNumber}");
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"Sender: {sender}");
+            if (sender != null)
+            {
+                sb.AppendLine($"Sender Type: {sender.GetType().FullName}");
+            }
+            sb.AppendLine("-------");
+            do
+            {
+                sb.AppendLine($"exception type: {ex.GetType().FullName}");
+                sb.AppendLine($"exception message: {ex.Message}");
+                sb.AppendLine($"exception stacktrace: {ex.StackTrace}");
+                sb.AppendLine("-------");
+                ex = ex.InnerException;
+            } while (ex != null);
+
+            MessageBox.Show(sb.ToString(), "unhandled exception :-(");
+            Shutdown(-1);
         }
     }
 }
