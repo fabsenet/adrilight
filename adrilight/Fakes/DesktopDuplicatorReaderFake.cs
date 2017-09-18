913ee956d8e6fd44d230b73197e54d793462a437
Fabian Wetzel <fabsenet@googlemail.com>
2017-09-18 21:43:43 +0200

    improved dependency injection and provided fakes for design time

---
+++ adrilight/Fakes/DesktopDuplicatorReaderFake.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace adrilight.Fakes
+{
+    class DesktopDuplicatorReaderFake : IDesktopDuplicatorReader
+    {
+        public bool IsRunning { get; set; }= false;
+
+        public void Run(CancellationToken token)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                IsRunning = true;
+                try
+                {
+                    token.WaitHandle.WaitOne();
+                }
+                finally
+                {
+                    IsRunning = false;
+                }
+            });
+        }
+    }
+}
--- adrilight/IDesktopDuplicatorReader.cs
+++ adrilight/IDesktopDuplicatorReader.cs
@@ -8,7 +8,6 @@ namespace adrilight
     {
         bool IsRunning { get; }
 
-        void GetAverageColorOfRectangularRegion(Rectangle spotRectangle, int stepy, int stepx, BitmapData bitmapData, out int sumR, out int sumG, out int sumB, out int count);
         void Run(CancellationToken token);
     }
 }
