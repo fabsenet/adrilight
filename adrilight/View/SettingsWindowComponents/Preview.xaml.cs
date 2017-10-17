b6ffdc955829322c5b3c6cab0a633e5cc26109a1
Fabian Wetzel <fabsenet@googlemail.com>
2017-10-17 22:42:28 +0200

    started to provide early,simple UI

---
+++ adrilight/View/SettingsWindowComponents/Preview.xaml.cs
@@ -0,0 +1,47 @@
+﻿using adrilight.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace adrilight.View.SettingsWindowComponents
+{
+    /// <summary>
+    /// Interaction logic for LightingMode.xaml
+    /// </summary>
+    public partial class Preview : UserControl
+    {
+        public Preview()
+        {
+            InitializeComponent();
+        }
+
+
+
+        public class LightingModeSetupSelectableViewPart : ISelectableViewPart
+        {
+            private readonly Lazy<Preview> lazyContent;
+
+            public LightingModeSetupSelectableViewPart(Lazy<Preview> lazyContent)
+            {
+                this.lazyContent = lazyContent ?? throw new ArgumentNullException(nameof(lazyContent));
+            }
+
+            public int Order => 700;
+
+            public string ViewPartName => "Preview Results";
+
+            public object Content { get => lazyContent.Value; }
+        }
+    }
+}
