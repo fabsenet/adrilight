c73c2941e99e5b105c5f993811108d47614e829c
Fabian Wetzel <fabsenet@googlemail.com>
2017-12-12 20:48:49 +0100

    started the new preview tab

---
+++ adrilight/Messaging/TraceFrameMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adrilight.Messaging
+{
+    struct TraceFrameMessage
+    {
+        public int Width;
+        public int Height;
+    }
+}
